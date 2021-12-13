43ad66729b658f937e62ab00eb47626a001fd59d
Ajuaman <tate.chin24@gmail.com>
2021-12-13 11:59:50 -0500

    Commit Test

--- Sketch.java
+++ Sketch.java
@@ -2,29 +2,16 @@ import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
   public void settings() {
-	// put your size call here
     size(400, 400);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
   public void setup() {
     background(210, 255, 173);
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
   public void draw() {
 	  
-	// sample code, delete this stuff
     stroke(128);
     line(150, 25, 270, 350);  
 
@@ -32,5 +19,4 @@ public class Sketch extends PApplet {
     line(50, 125, 70, 50);  
   }
   
-  // define other methods down here.
 }
